18e917aa7c3149b44ac67f32b4cb0766184a1ac3
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-05-20 14:29:44 -0400

    add: components to populate sample-data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
+import sampleData from "../../db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 
 export const metadata = {
   title: 'home'
 }
 
 const Homepage = () => {
-  
-  return ( <>EZ Store</> );
+  // implement a limit of 4 products on screen
+  return ( <div>
+    <ProductList 
+      data={sampleData.products} 
+      title="Newest Arrivals" 
+      limit={4}
+    />
+  </div> );
 }
  
 export default Homepage;
---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,22 @@
+const ProductList = ({data, title, limit}: {data: any; title?: string; limit?: number}) => {
+         // will create Product type for ^data param
+         const limitedData = limit ? data.slice(0, limit) : data//data limit
+    return ( 
+        <div className="my-10">
+            <h2 className="h2-bold mb-4">{title}</h2>
+            {data.length > 0 ? (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    {limitedData.map((product: any) => (
+                        <div>{product.name}</div>
+                    ))}
+                </div>
+            ) : (
+                <div>
+                <p>No Products Found</p>
+                </div>
+            ) }
+        </div>
+     );
+}
+ 
+export default ProductList;
